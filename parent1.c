5e2c3560f93e3431622725c7c9a6cc1a387f12bd
Suhas Pai <cs17b116@smail.iitm.ac.in>
2019-11-03 18:54:07 +0530

    Play around with fork children

--- parent1.c
+++ parent1.c
@@ -6,19 +6,48 @@
 int
 main(int argc, char *argv[])
 {
-  char *argv_1[] = {"my_prog",0 };
-  char *argv_2[] = {"race_cond",0}; 
-  init();
-  int pid = fork();
-  if(pid == 0)
+  int  i=0;
+  int st = uptime();
+  //int all_wait_time = 0;
+  for(i=0;i<10;i++)
   {
-    exec("my_prog1",argv_1);
-   
+    int pid = fork();
+    if(pid==0)
+    {
+      int tot_time = 0;
+      int real_time = uptime();
+    for(int i=0;i<10000;i++)
+    {
+      //printf(1,"%d\n",i);
+      int t = my_lock();
+      tot_time+=t;
+      //int st = uptime();
+      //printf(1,"%d\n",i);
+      int g = get();
+      set(g+1);
+
+      //printf(1,"%d\n",i );
+      my_unlock();
+      //yield();
+      //printf(1,"%d %d\n",i,g ); 
+    }
+    //int x = get();
+    real_time = uptime() - real_time;
+    real_time = real_time *10;
+    //int end = uptime();
+    //end = (end-st)*10;
+    //printf(1,"Total waiting time in microseconds is %d\n", tot_time*10);
+    //printf(1,"%Total execution time in microseconds is %d\n",real_time);
+    //printf(1,"%d\n",x);
+    exit();
+    }
+
   }
-  else
-  {
-    exec("race_cond1",argv_2); 
-  }
+  for(int i=0;i<10;i++)
+  wait();
+  int end = uptime() - st;
+  end = end *10;
+  printf(1,"Total execution time is %d\n",end);
   
  
 }
